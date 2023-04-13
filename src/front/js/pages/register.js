f34a41dd20ea5cfaa1ac1ae43626bf64e7f1112e
irenecervillavalle <irenecervillavalle@gmail.com>
2023-04-13 17:09:41 +0000

    new commit

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -20,7 +20,7 @@ export const Register = () => {
     };
 
     const URL =
-      "https://3001-irenecervill-ladybikers-coztfh0jqwh.ws-eu90.gitpod.io/api";
+      "https://3001-irenecervill-ladybikers-3t7f3rf50xs.ws-eu94.gitpod.io/api";
 
     fetch(`${URL}/signup`, {
       headers: {
